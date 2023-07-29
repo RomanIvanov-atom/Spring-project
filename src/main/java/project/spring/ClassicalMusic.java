159a7df67287b704a1ae7b6d29ae3cf3b73d28ad
Roman Ivanov <atom10121998@mail.ru>
2023-07-29 23:58:28 +0300

    v 1.6.1.0 | Add annotation "Component" without id name

--- src/main/java/project/spring/ClassicalMusic.java
+++ src/main/java/project/spring/ClassicalMusic.java
@@ -2,7 +2,7 @@ package project.spring;
 
 import org.springframework.stereotype.Component;
 
-@Component("musicBean")
+@Component
 public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
