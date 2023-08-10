22ecda0dcb3a9e436b93ff535fb5cce7b7dd5fa1
Roman Ivanov <atom10121998@mail.ru>
2023-08-10 19:05:38 +0300

    v 5.0.0.0 | CRUD, REST, Pattern DAO with DB as ArrayList

---
+++ src/main/java/projectMVC/spring/controllers/PeopleController.java
@@ -0,0 +1,36 @@
+package projectMVC.spring.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import projectMVC.spring.dao.PersonDAO;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id,
+                       Model model) {
+        // Получим 1 человека по его id из DAO и передадим его отображение в VIEW
+        model.addAttribute("person", personDAO.show(id));
+        //model.addAttribute("person", personDAO.index().get(id));
+        return "people/show";
+    }
+}
